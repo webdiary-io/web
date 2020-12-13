c82d896fd6d712c4c0f733cabcee5ee3c6485cf8
Roman Mikhailov <roman.mikhailov@barco.com>
2020-12-13 15:57:28 -0500

    tsc strict false, add type safety

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,18 +1,22 @@
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 import Layout from "../../components/layout";
 import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
@@ -20,14 +24,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths,
     fallback: false,
   };
-}
+};
 
-export default function Post({ postData }: {
+export default function Post({
+  postData,
+}: {
   postData: {
-    title: string
-    date: string
-    contentHtml: string
-  }}) {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
